eab2e935d311b24702f3f0c749c5479dc35d626a
Ralves34 <lucasalves34400@gmail.com>
2022-12-02 19:12:27 +0100

    Ouvrir et lire un fichier CSV

--- script_parsing.py
+++ script_parsing.py
@@ -1,2 +1,15 @@
 #début script
+import csv
+table=[]
+col_addition = 0
+col_addition_2 = 0
+
+
+#lire le fichier csv source
+with open('conso-annuelles.csv', 'r') as f:
+    obj = csv.reader(f,delimiter=';')
+
+#extraire données vers fichier csv
+    with open('conso-clan.csv', 'w',newline='') as resultat:
+        writer = csv.writer(resultat,delimiter=';')
 
